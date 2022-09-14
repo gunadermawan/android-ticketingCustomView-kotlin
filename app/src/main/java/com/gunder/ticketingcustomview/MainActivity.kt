ae30e5fb6dd15667541add8a6e88349c7e8087f0
itselaras <gunadermawan62@gmail.com>
2022-09-15 05:58:51 +0700

    add touch event

--- app/src/main/java/com/gunder/ticketingcustomview/MainActivity.kt
+++ app/src/main/java/com/gunder/ticketingcustomview/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.gunder.ticketingcustomview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.gunder.ticketingcustomview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val seatsView = binding.seatsView
+        val seatButton = binding.btnFinish
+        seatButton.setOnClickListener {
+            seatsView.seat?.let {
+                Toast.makeText(this, "Kursi Anda nomor ${it.name}", Toast.LENGTH_SHORT).show()
+            } ?: run {
+                Toast.makeText(this, "Silahkan pilih kursi terlebih dahulu!", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 }
